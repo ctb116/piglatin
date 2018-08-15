1a1ee0d5dc9d1fb9ee7b197f3f380ba1a5070be1
Catherine Bradley and David Mortkowitz <david@>
2018-08-15 11:52:21 -0700

    add function to detect if first letter in string is a vowel and return true or false

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 //Backend
 var vowels = ["a", "e", "i", "o", "u"];
 
-var firstVowel = function(userInput) {
-  alert("backend works")
-
-}
+function firstVowel(string) {
+  // for (var i = 0; i < userInput.length; i++) {
+    if (vowels.includes(string.charAt(0))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 // for (var index = 0; index = 1; index += 1) {
 //   var firstvowel = (userInput + "way");
@@ -21,10 +25,6 @@ $(document).ready(function(){
   var userInput = $("input#inputText").val();
 
   var result = firstVowel(userInput)
-
-
-
-
 
   $(".output").text(result);
 
