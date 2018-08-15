d13f5c9e58d6ca81ae7c09914f351a7be150707a
Catherine Bradley and David Mortkowitz <david@>
2018-08-15 10:40:03 -0700

    Add ability to add phrase way to end of words.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 $(document).ready(function(){
   $("form#submitForm").submit(function(event) {
-    event.preventDefault();
+  event.preventDefault();
   var userInput = $("input#inputText").val();
-  $(".output").text(userInput);
+  $(".output").text(userInput + "way");
 
 
 
   });
+
+// var vowels = ["a", "e", "i", "o", "u"];
+// for (var index = 0; index = 1; index += 1) {
+//   var vowels = ["a", "e", "i", "o", "u"];
+//
+// }
+
+
+
 });
