a0d703be1d476ec12afb67eaeb26d335aafa002a
Catherine Bradley and David Mortkowitz <catherine@>
2018-08-15 10:52:54 -0700

    Add backend functionality for user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
-$(document).ready(function(){
-  $("form#submitForm").submit(function(event) {
-  event.preventDefault();
-  var userInput = $("input#inputText").val();
-  $(".output").text(userInput + "way");
+//Backend
+var vowels = ["a", "e", "i", "o", "u"];
 
+var firstVowel = function(userInput) {
+  alert("backend works")
 
+}
 
-  });
-
-// var vowels = ["a", "e", "i", "o", "u"];
 // for (var index = 0; index = 1; index += 1) {
-//   var vowels = ["a", "e", "i", "o", "u"];
+//   var firstvowel = (userInput + "way");
 //
 // }
 
 
 
+//Frontend
+$(document).ready(function(){
+  $("form#submitForm").submit(function(event) {
+  event.preventDefault();
+
+  var userInput = $("input#inputText").val();
+
+  var result = firstVowel(userInput)
+
+
+
+
+
+  $(".output").text(result);
+
+  });
 });
