1671bc557508a274bc6cb5b4b4ad51d05deff340
Catherine Bradley and David Mortkowitz <catherine@>
2018-08-16 16:41:16 -0700

    new changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,43 @@
 //Backend
-var vowels = ["a", "e", "i", "o", "u", "y"];
+// var vowels = ["a", "e", "i", "o", "u", "y"];
+//   var piglatin = [];
 
-function firstStep(string) {
-  var piglatin;
+//   function firstStep(array) {
+//     var translatedArray = []
+//
+//   }
+//
+//   array.forEach(function(vowels) {
+//     console.log(vowels + "way");
+// });
+//   for (var index = 0; index < firstLetter.length; index+=1) {
+//     var first = firstLetter[-1,1];
+//     if (first.match(/[aeiou]/g)); {
+//       piglatin = firstLetter + "way"
+//     }
+//
+//   }
+//   console.log(piglatin);
+// }
 
-    if (vowels.includes(string.charAt(0))) {
-     piglatin = string + "way";
-    } else {
-      consonants(string);
-    }
 
-  function consonants(string) {
-    // for (var index = 0; index < string.length; index+=1);
-      if (string.indexOf(string.match(vowels))) {
-        var hasvowels = string.indexOf(vowels);
-        alert(hasvowels);
-      } else {
-        alert("there are no vowels");
-      }
-  }
-    console.log(piglatin);
-  }
+  //   if (vowels.includes(string.charAt(0))) {
+  //    piglatin = string + "way";
+  //   } else {
+  //     consonants(string);
+  //   }
+  //
+  // function consonants(string) {
+  //   // for (var index = 0; index < string.length; index+=1);
+  //     if (string.indexOf(string.match(vowels))) {
+  //       var hasvowels = string.indexOf(vowels);
+  //       alert(hasvowels);
+  //     } else {
+  //       alert("there are no vowels");
+  //     }
+  // }
+  //   console.log(piglatin);
+  // }
 
   // string.substring(0, vowels);
   // function consonants(string) {
@@ -39,10 +56,22 @@ $(document).ready(function(){
   $("form#submitForm").submit(function(event) {
   event.preventDefault();
 
-  var userInput = $("input#inputText").val();
+  var firstInput = $("#inputText").val();
+  var userInput = firstInput.split(" ");
 
-  firstStep(userInput)
+  console.log(userInput);
 
+  userInput.forEach(function(vowel) {
+    var list = ["a", "e", "i", "o", "u"]
+    if (list.includes(vowel.charAt(0))) {
+            console.log(vowel + "way");
+    } else {
+      console.log("no");
+    }
+
+  });
+
+  // console.log(userInput);
 
   // $(".output").text(piglatin);
   // $(".output").text(con);
